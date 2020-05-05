039e06f286ce4304833d58a244296fc6e3b0222e
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-05 18:52:33 +0200

    TD: Using ngModel with Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   // @ViewChild('f') signUpForm: NgForm; // Pre @Angular 8
   @ViewChild('f', {static: false}) signUpForm: NgForm; // From @Angular 8
   defaultQuestion = 'teacher';
+  answer = "";
 
   suggestUserName() {
     const suggestedName = 'Superuser';
